1c3db5777648306e8c65532351aa17a9c0f808ad
Abdi-01 <abdialghi@gmail.com>
2024-12-26 14:30:18 +0700

    revam setup deploy in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     @vite('resources/css/app.css')
 
-    <link rel="stylesheet" href="{{ asset('assets/app*.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/app-BmI-86B9.css') }}">
 
 </head>
 
